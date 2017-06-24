830f40eef06b4c4de7f9f61383e9363ed59dc011
lzdn <415656544@qq.com>
2017-06-24 11:55:27 +0800

    first commit

---
+++ js/constant.js
@@ -0,0 +1,6 @@
+//http://localhost:8089/zlwx/webapi/testRestFulService/hello
+var STATIC_PREFIX="http://";//https
+var STATIC_IP="127.0.0.1";
+var STATIC_PORT="8089";
+var STATIC_PROJECT_NAME="zlwx";
+var STATIC_URL=STATIC_PREFIX+STATIC_IP+":"+STATIC_PORT+"/"+STATIC_PROJECT_NAME;
